26ff5b687624fdfd51191ea1a21315a708912120
Hưng Nguyễn <hungnguyen101196.com>
2023-08-13 03:20:18 +0700

    resolve feedback

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { Request, Response } from "express";
 
 (async () => {
 
@@ -9,17 +10,17 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
   app.use(bodyParser.json());
 
-  app.get("/", async (req, res) => {
+  app.get("/", async (req: Request, res: Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   });
 
-  app.get("/filteredimage", async (req, res) => {
-    let image_url = req.query.image_url;
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    let image_url: string = req.query.image_url;
     if (!image_url) {
       res.status(400).send('Bad request: url for image is required')
     }
     filterImageFromURL(image_url).then(result => {
-      res.sendFile(result,
+      res.status(200).sendFile(result,
         function (err) {
           if (!err) {
             deleteLocalFiles([result]);
